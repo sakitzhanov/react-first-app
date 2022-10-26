850af4ed82fd42dc6f6ac0c9b7d7588a3fe42e5b
sakitzhanov <a.sakitzhanov@gmail.com>
2022-10-26 18:23:32 +0600

    Dialogs changed

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,29 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div className={classes.content}>
-            Ulala!
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Asset
+                </div>
+                <div className={classes.dialog}>
+                    Alua
+                </div>
+                <div className={classes.dialog}>
+                    Temirlan
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    HI!
+                </div>
+                <div className={classes.message}>
+                    Answer please...
+                </div>
+                <div className={classes.message}>
+                    =\
+                </div>
+            </div>
         </div>
     )
 }
